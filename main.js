064d4ed2e96a664248a142c38337a2e3a4a78452
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-15 13:13:33 +0000

    1502202301

---
+++ main.js
@@ -0,0 +1,12 @@
+const m1 = document.getElementById("m1");
+const btn = document.getElementById("btn");
+var last = document.getElementById("l1");
+
+btn.addEventListener("click", function () {
+  last.innerText = input.value;
+});
+m1.addEventListener("click", function () {
+  var l1 = parseInt(document.getElementById("l1").innerText) + parseInt(1);
+  document.location.href =
+    "https://manhuaplus.com/manga/martial-peak/chapter-" + l1;
+});
