5ec367113084aee53238b0a29c7c97820c6d0a48
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-15 19:02:11 +0000

    0.1.4

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-var title = document.getElementById("title").value;
-var last = document.getElementById("last").value;
-
 fetch('https://sheetdb.io/api/v1/aux5lgr4vfrx2?sort_by=id&sort_order=asc&cast_numbers=last')
   .then((response) => response.json())
   .then((data) => console.log(data));
