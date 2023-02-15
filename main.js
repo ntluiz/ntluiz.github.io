b41cb8aef5cdf8e799782733ec0a71db68e7120d
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-15 17:18:28 +0000

    0.0010

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-const m1 = document.getElementById("m1");
-const btn = document.getElementById("btn");
-var last = document.getElementById("l1");
-
-btn.addEventListener("click", function () {
-  last.innerText = input.value;
-});
-m1.addEventListener("click", function () {
-  var l1 = parseInt(document.getElementById("l1").innerText) + parseInt(1);
-  window.open('https://manhuaplus.com/manga/martial-peak/chapter-' + l1, '_blank');
-});
-
-let url = 'https://api.sheety.co/3f2b4c50b08e3901f43d14db1494fa21/mangas/manga';
-fetch(url)
-  .then((response) => response.json())
-  .then(json => {
-    // Do something with the data
-    console.log(json.manga);
-  });
-
 $(document).ready(function () {
   $('#signup').submit(function (e) {
     e.preventDefault();
@@ -25,7 +5,7 @@ $(document).ready(function () {
     $.ajax({
       type: 'post',
       dataType: 'json',
-      url: form.attr('action'),
+      url: 'https://api.sheety.co/3f2b4c50b08e3901f43d14db1494fa21/mangas/manga',
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         manga: {
