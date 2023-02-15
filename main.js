997c2a6b6c2d18d85103ae21285aebbaad19b0c5
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-15 19:09:06 +0000

    0.1.10

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 fetch('https://sheetdb.io/api/v1/aux5lgr4vfrx2?sort_by=id&sort_order=asc&cast_numbers=last')
   .then((response) => response.json())
   .then((data) => console.log(data));
+
+// Add
+var form = document.getElementById('add');
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("add")),
+  }).then(
+    response => response.json()
+  ).then((html) => {
+    // you can put any JS code here
+    alert('Ok!')
+  });
+});
