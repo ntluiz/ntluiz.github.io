b353ceb1d6cbbf18e673510d680e4d07304269b3
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-15 16:46:26 +0000

    0.0006

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ m1.addEventListener("click", function () {
   var l1 = parseInt(document.getElementById("l1").innerText) + parseInt(1);
   window.open('https://manhuaplus.com/manga/martial-peak/chapter-'+ l1, '_blank');
 });
+
+let url = 'https://api.sheety.co/3f2b4c50b08e3901f43d14db1494fa21/mangas/manga';
+fetch(url)
+.then((response) => response.json())
+.then(json => {
+  // Do something with the data
+  console.log(json.manga);
+});
