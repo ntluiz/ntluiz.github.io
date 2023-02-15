ad9c9e82b104389caff9100ee6717b69ab6ce973
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-15 17:07:36 +0000

    0.0007

--- main.js
+++ main.js
@@ -17,3 +17,29 @@ fetch(url)
   // Do something with the data
   console.log(json.manga);
 });
+
+$(document).ready(function(){
+  $('#signup').submit(function(e) {
+	e.preventDefault();
+	let form = $(this);
+	$.ajax({
+	  type: 'post',
+	  dataType: 'json',
+	  url: form.attr('action'),
+	  contentType: 'application/json; charset=utf-8',
+	  data: JSON.stringify({
+		email: {
+		  name: $('#name').val(),
+		  email: $('#email').val()
+		} 
+	  }),
+	  success: function() {
+		$('#signup-success').show();
+		$('#signup').hide();
+	  },
+	  error: function(xhr, res, text) {
+		alert('There was a problem registering.');
+	  }
+	});
+  });
+});
