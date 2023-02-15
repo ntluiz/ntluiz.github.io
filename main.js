890bb3d5ef2738153cd5211ae1f3bc5469cf5fa8
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-15 17:11:39 +0000

    0.0009

--- main.js
+++ main.js
@@ -7,39 +7,39 @@ btn.addEventListener("click", function () {
 });
 m1.addEventListener("click", function () {
   var l1 = parseInt(document.getElementById("l1").innerText) + parseInt(1);
-  window.open('https://manhuaplus.com/manga/martial-peak/chapter-'+ l1, '_blank');
+  window.open('https://manhuaplus.com/manga/martial-peak/chapter-' + l1, '_blank');
 });
 
 let url = 'https://api.sheety.co/3f2b4c50b08e3901f43d14db1494fa21/mangas/manga';
 fetch(url)
-.then((response) => response.json())
-.then(json => {
-  // Do something with the data
-  console.log(json.manga);
-});
+  .then((response) => response.json())
+  .then(json => {
+    // Do something with the data
+    console.log(json.manga);
+  });
 
-$(document).ready(function(){
-  $('#signup').submit(function(e) {
-	e.preventDefault();
-	let form = $(this);
-	$.ajax({
-	  type: 'post',
-	  dataType: 'json',
-	  url: form.attr('action'),
-	  contentType: 'application/json; charset=utf-8',
-	  data: JSON.stringify({
-		email: {
-		  name: $('#name').val(),
-		  email: $('#email').val()
-		} 
-	  }),
-	  success: function() {
-		$('#signup-success').show();
-		$('#signup').hide();
-	  },
-	  error: function(xhr, res, text) {
-		alert('There was a problem registering.');
-	  }
-	});
+$(document).ready(function () {
+  $('#signup').submit(function (e) {
+    e.preventDefault();
+    let form = $(this);
+    $.ajax({
+      type: 'post',
+      dataType: 'json',
+      url: form.attr('action'),
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify({
+        manga: {
+          title: $('#title').val(),
+          last: $('#last').val()
+        }
+      }),
+      success: function () {
+        $('#signup-success').show();
+        $('#signup').hide();
+      },
+      error: function (xhr, res, text) {
+        alert('There was a problem registering.');
+      }
+    });
   });
 });
