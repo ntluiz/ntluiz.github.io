79c238fa5026f72883df2eaf74117d594b14a816
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-15 18:17:04 +0000

    0.0015

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+var title = document.getElementById("title").value;
+var last = document.getElementById("last").value;
 
-let url = 'https://api.sheety.co/3f2b4c50b08e3901f43d14db1494fa21/manga/manga';
+let url = 'https://api.sheety.co/3f2b4c50b08e3901f43d14db1494fa21/teste/log';
 let body = {
-  manga: {
-    title: $('#title').val(),
-    last: $('#last').val()
+  log: {
+    title: title,
+    last: last
   }
-};
+}
 fetch(url, {
   method: 'POST',
   body: JSON.stringify(body)
@@ -13,5 +15,5 @@ fetch(url, {
   .then((response) => response.json())
   .then(json => {
     // Do something with object
-    console.log(json.manga);
+    console.log(json.log);
   });
