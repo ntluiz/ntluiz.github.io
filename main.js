bea401f03eb1d15336e7593046d50220bb10d2c8
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-15 18:09:35 +0000

    0.0013

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
-$(document).ready(function () {
-  $('#signup').submit(function (e) {
-    e.preventDefault();
-    let form = $(this);
-    $.ajax({
-      type: 'post',
-      dataType: 'json',
-      url: 'https://api.sheety.co/3f2b4c50b08e3901f43d14db1494fa21/mangas',
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify({
-        manga: {
-          title: $('#title').val(),
-          last: $('#last').val()
-        }
-      }),
-      success: function () {
-        $('#signup-success').show();
-        $('#signup').hide();
-      },
-      error: function (xhr, res, text) {
-        alert('There was a problem registering.');
-      }
-    });
+
+let url = 'https://api.sheety.co/3f2b4c50b08e3901f43d14db1494fa21/manga/manga';
+let body = {
+  manga: {
+    title: $('#title').val(),
+    last: $('#last').val()
+  }
+};
+fetch(url, {
+  method: 'POST',
+  body: JSON.stringify(body)
+})
+  .then((response) => response.json())
+  .then(json => {
+    // Do something with object
+    console.log(json.manga);
   });
-});
