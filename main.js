ecd0a9adf9a1b3fdd2d4105d27007d016842bf22
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-16 13:09:28 +0000

    0.1.17

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ fetch('https://sheetdb.io/api/v1/aux5lgr4vfrx2?sort_by=id&sort_order=asc&cast_nu
   .then((data) => console.log(data));
 
 // Add
+
 var form = document.getElementById('add');
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -15,4 +16,30 @@ form.addEventListener("submit", e => {
     // you can put any JS code here
     alert('Ok!')
   });
+});
+
+// Update
+
+var update = document.getElementById('update');
+var urlupdate = 'https://sheetdb.io/api/v1/58f61be4dda40/title/' + document.getElementById('titleup').value;
+update.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(urlupdate, {
+    method: 'PATCH',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      data: {
+        'last': document.getElementById('lastup').value,
+      }
+    })
+  })
+    .then(
+      response => response.json()
+    ).then((html) => {
+      // you can put any JS code here
+      alert('Ok!')
+    });
 });
