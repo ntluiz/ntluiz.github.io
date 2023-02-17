e6463794934a32a661c00b24462fa74d63ddf49b
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-17 17:35:03 +0000

    0.5.1

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ fetch("https://sheetdb.io/api/v1/aux5lgr4vfrx2")
       const cid = document.createElement("h5");
       cid.className = "id";
       cid.textContent = product.id;
-      const lid = product.slice(-1);
+      var lid = product.slice(-1);
       console.log(lid);
       document.getElementById("linkinput").value = "=E" + lid + "&" + "C" + lid + "+1";
       select.append(
