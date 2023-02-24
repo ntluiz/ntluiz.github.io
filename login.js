58ceecc1e2a62ab4b190fb78d0ec4112d8014e96
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-02-24 13:41:19 -0300

    1.0.2

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
 function Login() {
-    var done=0;
-    var usuario = document.getElementsById('name')[0].value;
-    usuario=usuario.toLowerCase();
-    var senha= document.getElementsById('senha')[0].value;
-    senha=senha.toLowerCase();
-    if (usuario=="ntluiz" && senha=="0309") {
-      window.location="/estante.html";
-      done=1;
-    }
-    if (done==0) { alert("Dados incorretos, tente novamente"); }
+  var done = 0;
+  var usuario = document.getElementsById('name')[0].value;
+  usuario = usuario.toLowerCase();
+  var senha = document.getElementsById('senha')[0].value;
+  senha = senha.toLowerCase();
+  if (usuario == "ntluiz" && senha == "0309") {
+    window.location = "https://ntluiz.github.io/estante.html";
+    done = 1;
   }
+  if (done == 0) { alert("Dados incorretos, tente novamente"); }
+};
