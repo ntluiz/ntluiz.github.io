b0d501ec3021123f1d7a3dcd1475ae245489828f
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-03-22 18:27:55 +0000

    2.0.2

---
+++ leitor/leitor.js
@@ -0,0 +1,42 @@
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const up = document.getElementById("top");
+const scroll = document.getElementById("scroll");
+const title = document.getElementById("title");
+const input = document.getElementById("chapter");
+const ok = document.getElementById("ok");
+
+
+plus.addEventListener("click", function pageScroll() {
+    scroll.scrollBy(0, 15);
+    scrolldelay = setTimeout(pageScroll, 5000);
+});
+minus.addEventListener("click", function () {
+    clearTimeout(scrolldelay);
+});
+up.addEventListener("click", function () {
+    document.getElementById("scroll").scrollTo(0, 0);
+    document.getElementById("scroll").scrollBy(0, 0);
+});
+ok.addEventListener("click", function () {
+    var url = "https://freewebnovel.com/tyranny-of-steel/chapter-300.html";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var txt = xhttp.response;
+            var title1 = document.querySelector('[property="og:novel:chapter_name"]');
+            var text = document.querySelector(".txt");
+            document.getElementById("hide").innerHTML = xhttp.response;
+            var filhos = document.querySelector(".txt").childNodes.length - 3;
+            for (var i = 5; i < filhos; i++) {
+                var teste = document.createElement("p");
+                teste.innerHTML = document.querySelector(".txt").childNodes[i].innerText;
+                scroll.append(teste);
+            }
+            // console.log();
+            title1.innerHTML = document.querySelector('[property="og:novel:chapter_name"]').content;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+});
