877fb83eca7b166c4fcde10832a775dfa9b36e3f
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-04-14 17:27:50 +0000

    01

--- tabela.php
+++ tabela.php
@@ -1,17 +1,15 @@
 <?php
-            require_once 'conect.php';
-            $query = "SELECT * FROM manga ORDER BY date";
-            $result = mysqli_query($conexao, $query);
+require_once 'conect.php';
+$query = "SELECT * FROM manga ORDER BY date";
+$result = mysqli_query($conexao, $query);
 
-            // Exibir os registros em uma tabela
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<tr>";
-                echo "<td>" . $row['title'] . "</td>";
-                echo "<td>" . $row['last'] . "</td>";
-                echo "<td><a href=\"" . $row['link'] . "\" target=\"_blank\">Ler</a></td>";
-                echo "</tr>";
-            }
+while ($row = mysqli_fetch_assoc($result)) {
+echo "<tr>";
+echo "<td>" . $row['title'] . "</td>";
+echo "<td>" . $row['last'] . "</td>";
+echo "<td><a href=\"" . $row['link'] . "\" target=\"_blank\">Ler</a></td>";
+echo "</tr>";
+}
 
-            // Fechar a conexão com o banco de dados
-            mysqli_close($conexao);
-            ?>
+mysqli_close($conexao);
+?>
